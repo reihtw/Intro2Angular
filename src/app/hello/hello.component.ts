26657bad9dc1ede29b3690c30bc6508255bfd6a2
= <=>
2022-07-18 09:05:18 -0300

    Interpolation and Expression

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+  myname: string = 'Reihtw';
+  bitwiseOR: number = 2 | 5;
+  i = 0;
+  someVar = Math.random();
+
   constructor() { }
 
+  doSomeHeavyTask() {
+    console.log(`Called ${this.i++} times`)
+  }
+
   ngOnInit(): void {
+    setInterval(() => {
+      this.someVar = Math.random();
+    }, 50)
   }
 
 }
